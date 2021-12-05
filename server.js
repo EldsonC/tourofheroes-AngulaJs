395e1895f9f29ba0b79a005f16f357118fdc24be
eldson <eldson13@gmail.com>
2021-12-05 11:31:18 -0300

    Heroku-heroes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path')
 
 app.use(cors())
 
-app.use(express.static(__dirname + "/dist/tourofheroes"))
+app.use(express.static(__dirname + "/dist/tour-of-heroes"))
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + "/dist/tourofheroes/index.html"))
+    res.sendFile(path.join(__dirname + "/dist/tour-of-heroes/index.html"))
 })
 
 app.listen(process.env.PORT || 8000)
