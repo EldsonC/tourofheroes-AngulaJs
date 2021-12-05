30c0b37488919f94668257f8305bd4e5e8f3f989
eldson <eldson13@gmail.com>
2021-12-05 10:45:47 -0300

    tour-of-heroes

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Hulk' },
+      { id: 2, name: 'Batman' },
+      { id: 3, name: 'Iron-man' },
+      { id: 4, name: 'Thor' },
+      { id: 5, name: 'Spider-man' },
+      { id: 6, name: 'RubberMan' },
+      { id: 7, name: 'Dynama' },
+      { id: 8, name: 'Dr IQ' },
+      { id: 9, name: 'Magma' },
+      { id: 10, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
